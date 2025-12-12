54cef15e227bcd543f239d5a8293fd70c0504573
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-12-12 09:01:35 +0200

    fix: update background color in layout and adjust logo width for better visual consistency

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -19,7 +19,7 @@ export default async function Home({
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main
-        className={`flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start ${
+        className={`flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-zinc-50 dark:bg-black sm:items-start ${
           lang === "ar" ? "sm:items-end" : ""
         }`}
       >
@@ -88,7 +88,7 @@ export default async function Home({
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={16}
+              width={18}
               height={16}
             />
             {dict.navigation.deploy}
